dd1cb08df02e4f0ad953e1fc1a75f1e0eb543f7f
Leul Ayfokru <abateisking@gmail.com>
2025-09-11 07:04:58 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -177,6 +177,7 @@ const About = () => {
 {profile?.twitter_url && (
                       <Button variant="outline" size="icon" asChild>
                         <a href={profile.twitter_url} target="_blank" rel="noopener noreferrer">
+<?xml version="1.0" encoding="iso-8859-1"?>
                          <svg version="1.1" baseProfile="basic" id="Layer_1"
 	 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50"
 	 xml:space="preserve">
